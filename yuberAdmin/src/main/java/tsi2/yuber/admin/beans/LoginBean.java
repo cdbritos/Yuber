367b1f8100f6763d9fe18a0df8c372b2529c6dfd
cbritos <cbritos@technisys.com>
2016-11-14 18:28:20 -0300

    agrego jsf

---
+++ yuberAdmin/src/main/java/tsi2/yuber/admin/beans/LoginBean.java
@@ -0,0 +1,32 @@
+package tsi2.yuber.admin.beans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean implements Serializable{
+
+	private static final long serialVersionUID = 4703944310557973975L;
+
+	private String name;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
